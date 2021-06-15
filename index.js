95eda4d4787801e57502cb11357ae3255194c9d7
yuito-syz <yuito_sy1122@icloud.com>
2021-06-15 20:56:53 +0900

    changing

--- index.js
+++ index.js
@@ -64,3 +64,19 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
