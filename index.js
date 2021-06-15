52e7692e5dc50a60bc8c51a355ae4e423356610e
yuito-syz <yuito_sy1122@icloud.com>
2021-06-15 21:27:27 +0900

    changing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const request = require('request');
 const PAGE_ACCESS_TOKEN = process.env.EAAMItyS9DgUBAFQajzRbUSct3qmFXZAvXiPbSXEfvvwLQ32gmrqlk;
 
 // Imports dependencies and set up http server
@@ -13,6 +14,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
+    // Parse the request body from the POST
     let body = req.body;
   
     // Checks this is an event from a page subscription
@@ -79,4 +81,78 @@ function handlePostback(sender_psid, received_postback) {
 function callSendAPI(sender_psid, response) {
   
 }
+
+body.entry.forEach(function(entry) {
+
+  // Gets the body of the webhook event
+  let webhook_event = entry.messaging[0];
+  console.log(webhook_event);
+
+  // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
+
+});
+
+body.entry.forEach(function(entry) {
+
+  // Gets the body of the webhook event
+  let webhook_event = entry.messaging[0];
+  console.log(webhook_event);
+
+
+  // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
+
+  // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+  if (webhook_event.message) {
+    handleMessage(sender_psid, webhook_event.message);        
+  } else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
+  
+});
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
 
