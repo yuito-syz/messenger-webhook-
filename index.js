6361b8f0ac8fc36be68a0b15f1250ce217d56fc7
yuito-syz <yuito_sy1122@icloud.com>
2021-06-22 22:48:24 +0900

    First commit

--- index.js
+++ index.js
@@ -120,7 +120,6 @@ function handleMessage(sender_psid, received_message) {
   
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
-  
   } 
   
   // Sends the response message
