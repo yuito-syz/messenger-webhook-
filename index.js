af871fdd03ae0a443cda00fed42af1dd6bec9d85
yuito-syz <yuito_sy1122@icloud.com>
2021-06-22 13:35:36 +0900

    test

--- index.js
+++ index.js
@@ -79,3 +79,26 @@ app.get('/webhook', (req, res) => {
       }
     }
 });
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
