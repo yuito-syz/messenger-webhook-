d897a61f32a7b673e5ee544ec112acc9d4a52b15
yuito-syz <yuito_sy1122@icloud.com>
2021-06-22 13:52:24 +0900

    test

--- index.js
+++ index.js
@@ -102,3 +102,27 @@ function callSendAPI(sender_psid, response) {
     }
   }); 
 }
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    
+    // Creates the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+
+  } else if (received_message.attachments) {
+  
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+  
+  } 
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
